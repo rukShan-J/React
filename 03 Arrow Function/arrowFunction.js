784f174ad993680dadba364143a2f0517d07fcee
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-09-23 19:07:06 +0530

    Regular function has a function keyword, the arrow function does not have a function keyword.

---
+++ 03 Arrow Function/arrowFunction.js
@@ -0,0 +1,11 @@
+// Normal function
+function greet(name) {
+    return ('Hello ') + name;
+}
+console.log(greet('Rukshan'));
+
+// Arrow function
+const greetArrow = (name)=> {
+    return 'Hello ' + name + ' - From the Arrow Function';
+}
+console.log(greetArrow('Pubudu'));
