fba71d074ba8a437c8009383c8b326317c027573
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-09-27 18:55:46 +0530

    using the rest operator, got all the elements in an object

--- 04 Rest/Spread/Rest/arrayDestructing.js
+++ 04 Rest/Spread/Rest/arrayDestructing.js
@@ -1,7 +1,9 @@
+// ARRAY
 const arr1 = [1,2,3,4,5,];
 
 // const a = arr1[0];
 // const b = arr1[1];
+// Instead of substituting variables individually like above(lines-5,6), substituting variables in a single line like below(line-8)
 
 const [first,second,...rest_argumenst] = arr1;
 
@@ -10,3 +12,12 @@ const [first,second,...rest_argumenst] = arr1;
 console.log(first);
 console.log(second);
 console.log(rest_argumenst);
+
+//OBJECT
+const obj1 = {one:10, two:20, three:30};
+
+const {one,two,...rest_obj} = obj1;
+
+console.log(one);
+console.log(two);
+console.log(rest_obj);
