c2ec55468370d386ec6cb575b22627923b62b2e2
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-09-27 18:41:38 +0530

    using the rest operator, got all the elements in an array.

---
+++ 04 Rest/Spread/Rest/arrayDestructing.js
@@ -0,0 +1,12 @@
+const arr1 = [1,2,3,4,5,];
+
+// const a = arr1[0];
+// const b = arr1[1];
+
+const [first,second,...rest_argumenst] = arr1;
+
+// console.log(a,b);
+
+console.log(first);
+console.log(second);
+console.log(rest_argumenst);
