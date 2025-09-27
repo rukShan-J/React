059d21966e5e4618ef0604bebee56b9b1a9e2450
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-09-27 22:04:51 +0530

    Add example showing string concatenation and template literals

---
+++ 05 Temolate Literals/interpretion.js
@@ -0,0 +1,5 @@
+const name = 'Rukshan';
+const age = 24;
+
+console.log('Hi, my name is ' +name + '. ' + 'My age is ' + age + 'years.');
+console.log(`Hi, my name is ${name}. My age is ${age} years.`);
