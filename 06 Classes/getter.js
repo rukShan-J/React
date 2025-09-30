5b0c3233b1ac22754b686418f84f87bc4a7cc763
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-09-30 19:11:44 +0530

    Implemented setter for area to update width and height based on new area
    Enhanced getArea()
    
    Updated example usage to demonstrate getter and setter functionality

--- 06 Classes/getter.js
+++ 06 Classes/getter.js
@@ -5,15 +5,26 @@ class square {
     }
 
     getArea() {
+        console.log(this.width, this.height);
         return this.width * this.height;
     }
 
     get area() {        // area is like this.area that inside this.width, this.height inside the constructor
         return this.width * this.height;
     }
+
+    set area(newArea) {
+        this.width = Math.sqrt(newArea);
+        this.height = Math.sqrt(newArea);
+    }
 }
 
 const newSquare = new square(10);
 
-console.log(newSquare.getArea());
+// console.log(newSquare.getArea());
 console.log(newSquare.area); // area is not call as a function
+
+newSquare.area = 25;
+
+console.log(newSquare.area);
+console.log(newSquare.getArea());
