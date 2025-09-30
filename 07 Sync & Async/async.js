dbe3e8d9cc855894dd2f417ad2c53cdf92a8e02a
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-09-30 23:44:25 +0530

    Add async program with setTimeout

---
+++ 07 Sync & Async/async.js
@@ -0,0 +1,15 @@
+function oneToFive() {
+   console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5); 
+}
+console.log('start');
+setTimeout(oneToFive,3000);  // setTimeout is a asynchronous function
+
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
