4417ad728cdafd2198aaa499d1bf945fef7f1c73
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-09-30 18:47:53 +0530

    Added - get area( )

--- 06 Classes/getter.js
+++ 06 Classes/getter.js
@@ -7,8 +7,13 @@ class square {
     getArea() {
         return this.width * this.height;
     }
+
+    get area() {        // area is like this.area that inside this.width, this.height inside the constructor
+        return this.width * this.height;
+    }
 }
 
 const newSquare = new square(10);
 
 console.log(newSquare.getArea());
+console.log(newSquare.area); // area is not call as a function
