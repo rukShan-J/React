11c26e9d442cf50e4cf67de1df1f52429d819353
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-10-05 14:07:56 +0530

    Implement promise chaining for sequential tasks
    - Modified takeShower(), doHomework(), and play() functions  to returning Promises.
    - Implemented .then() chaining to run tasks in sequence.
    - Added proper logging of each task result.
    -nAdded .catch() block for error handling.

--- 09 Promises/promises.js
+++ 09 Promises/promises.js
@@ -1,30 +1,49 @@
 function takeShower() {
-    // return new Promise((resolve, reject)=> {
+    return new Promise((resolve, reject)=> {
         setTimeout(()=>{
             console.log('You take shower');
-            // resolve('You took shower');
+            resolve('You took shower');
         },2000);
-    // });
+    });
 }
 
 function doHomework() {
-    // return new Promise((resolve, reject)=> {
+    return new Promise((resolve, reject)=> {
         setTimeout(()=>{
             console.log('You do your homework');
-            // resolve('You completed your homework');
+            resolve('You completed your homework');
         },1000);
-    // });
+    });
 }
 
 function play() {
-    // return new Promise((resolve, reject)=> {
+    return new Promise((resolve, reject)=> {
         setTimeout(()=>{
             console.log('You play what you like');
-            // resolve('You played what you like');
+            resolve('You played what you like');
         },3000);
-    // });
+    });
 }
 
-takeShower();
-doHomework();
-play();
+// call promises in sequence
+takeShower()
+.then((result) => {     // The code inside resolve is the output here.
+    console.log(result);
+    return doHomework();
+})
+.then((result) => {
+    console.log(result);
+    return play();
+})
+.then((result) => {
+    console.log(result);
+    console.log("All done.");
+})
+.catch((error) => {
+    console.error(error);
+});
+
+
+// takeShower();
+// doHomework();
+// play();
