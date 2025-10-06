80937afca72538b7768e0e578d69ad7c8fe2d1e4
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-10-06 15:55:56 +0530

    output not showing in browser (in frontend),
    But the output can be seen in the output area of the code executor.

--- 11 ES Modules/index.js
+++ 11 ES Modules/index.js
@@ -1,5 +1,6 @@
-// const extra = require('./extra.js');
+// because of the 'require' keyword, the output can be seen in the output area of ​​the code executor.
+const extra = require('./extra.js'); 
 
-// console.log(extra.text);
+console.log(extra.text);
 
 console.log('test log from index.js')
