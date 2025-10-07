3dbfdb721a1a2896654e36bf2a90715913702c05
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-10-07 23:22:13 +0530

    Add compose function to combine multiple functions into a single call
    * Implements `compose()` to take multiple functions as arguments
    * Returns a new function that applies all functions sequentially to an input
    * Demonstrates usage by creating `allInOneFunction` and computing result for input `5`
    * Shows how function composition simplifies calling multiple functions in sequence

--- 13 Simple React Concepts/composition.js
+++ 13 Simple React Concepts/composition.js
@@ -11,3 +11,20 @@ function substract1(x) {
 // let y = substract1(5);       // call one function
 let y = substract1(multiplyBy3(add2(5)));   // call all functions
 console.log(y);
+
+function compose() {
+    var functions = arguments;
+    return function(input) {
+        var value = input;
+        for(var i=0; i<functions.length; i++) {
+            value = functions[i](value);
+        }
+        return value;
+    }
+}
+
+var allInOneFunction = compose(add2,multiplyBy3,substract1);
+
+var result = allInOneFunction(5);
+
+console.log(result);
