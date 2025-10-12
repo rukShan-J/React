4c86d1d960cd1f99b05a4cc59e3ffd55393cd05d
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-10-12 13:35:59 +0530

    Created a copy of the above Install_Prettier project

---
+++ 19-Install_ESLint/src/App.js
@@ -0,0 +1,58 @@
+// Add a Student component
+const Student = () => {
+  return React.createElement(
+    "div", // Parent div
+    {}, // No props
+    [
+      // Child elements (array of React elements)
+      React.createElement("h3", {}, "Amal"), // Name
+      React.createElement("h3", {}, 20), // Age
+      React.createElement("h3", {}, "Gampaha"), // City
+    ],
+  );
+};
+
+// Add a StudentCard component
+const StudentCard = () => {
+  return React.createElement(
+    "div", // Parent wrapper
+    { className: "st-card" },
+    [
+      // Image element
+      React.createElement("img", {
+        src: "https://i.pinimg.com/736x/9e/43/2d/9e432dd715a57ffa3f7759a4e569655a.jpg",
+        alt: "Student Image",
+        className: "student-image",
+      }),
+      // Nested Student component
+      React.createElement(Student),
+    ],
+  );
+};
+
+// Main App component
+const App = () => {
+  // Define a React component using a function
+  return React.createElement(
+    "div", // HTML element type
+    {}, // Props (empty in this case)
+    // Child elements
+    [
+      React.createElement("h1", {}, "My React App"),
+      React.createElement(StudentCard),
+      React.createElement(StudentCard),
+      React.createElement(StudentCard),
+      React.createElement(StudentCard),
+      React.createElement(StudentCard),
+    ],
+  );
+};
+
+// Get the root DOM node
+const container = document.getElementById("root");
+
+// Create a React root for rendering (React 18+ feature)
+const root = ReactDOM.createRoot(container);
+
+// Render the App component inside the root element
+root.render(React.createElement(App));
