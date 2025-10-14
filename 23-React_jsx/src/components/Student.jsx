97f1df9513e5d788bd181f460bf2332910ccb96f
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-10-14 21:05:33 +0530

    refactor React components using JSX syntax
    - Converted Student and StudentCard components from React.createElement to JSX
    - Updated StudentCard to import and use Student component
    - Simplified App component to use JSX instead of React.createElement
    - Linked main entry point through index.html with App.jsx

---
+++ 23-React_jsx/src/components/Student.jsx
@@ -0,0 +1,23 @@
+// const Student = (props) => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     [
+//       React.createElement("h3", {}, props.name),
+//       React.createElement("h3", {}, props.age),
+//       React.createElement("h3", {}, props.homeTown),
+//     ],
+//   );
+// };
+
+const Student = () => {
+    return(
+        <div>
+            <h3>Amal</h3>
+            <h3>23</h3>
+            <h3>Gampaha</h3>
+        </div>
+    )
+};
+
+export default Student;
