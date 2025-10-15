bc699dfe39f00bb70778da29323384fb85325080
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-10-15 13:42:34 +0530

    render multiple StudentCard components with different props in App.jsx

--- 23-React_jsx/src/App.jsx
+++ 23-React_jsx/src/App.jsx
@@ -28,7 +28,9 @@ const App = () => {
     <div>
       <h1>My React App</h1>
       {/* <StudentCard /> */}
-      <StudentCard name="Amal" age={20} homeTown="Gampaha"/>
+      <StudentCard name="Amal" age={20} homeTown="Gampaha"/>;
+      <StudentCard name="Kamal" age={21} homeTown="Negombo"/>;
+      <StudentCard name="Nimal" age={22} homeTown="Colombo"/>
     </div>
   );
 };
