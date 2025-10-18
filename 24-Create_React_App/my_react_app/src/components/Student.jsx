f630447f72f2b1335ef05bd51c9277246ead8f20
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-10-18 20:03:10 +0530

    removed - before commented unnessasary codes from Student.jsx, and StudentCard.jsx

--- 24-Create_React_App/my_react_app/src/components/Student.jsx
+++ 24-Create_React_App/my_react_app/src/components/Student.jsx
@@ -1,35 +1,13 @@
-// const Student = (props) => {
-//   return React.createElement(
-//     "div",
-//     {},
-//     [
-//       React.createElement("h3", {}, props.name),
-//       React.createElement("h3", {}, props.age),
-//       React.createElement("h3", {}, props.homeTown),
-//     ],
-//   );
-// };
-
 import React from "react";
 
-// const Student = () => {
-//     return(
-//         <div>
-//             <h3>Amal</h3>
-//             <h3>23</h3>
-//             <h3>Gampaha</h3>
-//         </div>
-//     )
-// };
-
-    const Student = ({name,age,homeTown}) => {
-        return(
+const Student = ({name,age,homeTown}) => {
+    return(
         <div>
             <h3>{name}</h3>
             <h3>{age}</h3>
             <h3>{homeTown}</h3>
         </div>
     )
-    }
+}
 
 export default Student;
--- 24-Create_React_App/my_react_app/src/components/StudentCard.jsx
+++ 24-Create_React_App/my_react_app/src/components/StudentCard.jsx
@@ -1,18 +1,3 @@
-// const StudentCard = (props) => {
-//   return React.createElement(
-//     "div",
-//     { className: "st-card" },
-//     [
-//       React.createElement("img", {
-//         src: "https://i.pinimg.com/736x/9e/43/2d/9e432dd715a57ffa3f7759a4e569655a.jpg",
-//         alt: "Student Image",
-//         className: "student-image",
-//       }),
-//       React.createElement(Student, {...props}, null),
-//     ]
-//   );
-// };
-
 import Student from "../components/Student";
 import React from "react";
 
@@ -23,7 +8,6 @@ const StudentCard = ({name,age,homeTown}) => {
             src="https://i.pinimg.com/736x/9e/43/2d/9e432dd715a57ffa3f7759a4e569655a.jpg" 
             alt={`Student Image_${name}`} 
             className="student-image" />
-            {/* <Student /> */}
             <Student name={name} age={age} homeTown={homeTown}/>
         </div>
     )
