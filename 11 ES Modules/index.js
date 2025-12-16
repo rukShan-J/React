83c8642fbc5fa9de78208e9029124ea83d8d0b7f
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-12-16 20:10:18 +0530

    import, add txt
    import {readFile} from 'fs/promises'; in index.html
    
    Add Sample text

--- 11 ES Modules/index.js
+++ 11 ES Modules/index.js
@@ -4,7 +4,16 @@
 // console.log(extra.text);
 
 import {text,hello} from './extra.js';
+import {readFile} from 'fs/promises';
 
 console.log(text);
 
 console.log('test log from index.js');
+
+
+async function txtFile() {
+    var data = await readfile('./sample.txt','utf8');
+    console.log(data);
+}
+
+txtFile();
