ae174413eccebf7d851dae0488cb1dd4546974cf
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-12-22 21:58:27 +0530

    copy of project 25... and named as 26...

---
+++ 26-Handler_Function/my_react_app/src/components/Student.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Student = ({name,age,homeTown}) => {
+    return(
+        <div>
+            <h3>{name}</h3>
+            <h3>{age}</h3>
+            <h3>{homeTown}</h3>
+        </div>
+    )
+};
+
+Student.prototype = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    homeTown: PropTypes.string,
+};
+
+export default Student;
