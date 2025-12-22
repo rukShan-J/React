0259a5bd6d3b89860108b1f090edf8b1c5ccfb4e
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-12-22 23:07:51 +0530

    add text input and handleChange function to log values

--- 26-Handler_Function/my_react_app/src/App.js
+++ 26-Handler_Function/my_react_app/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  
+
+  const handleChange = (event) =>{
+    console.log(event.target.value);
+  }
+
   return (
     <div>
       <h1>My React App</h1>
-      <input type="text" id="normal-text-2"></input>
+      <input type="text" id="normal-text-2" onChange={handleChange}></input>
       <StudentCard name="Amal" age={20} homeTown="Gampaha"/>
       <StudentCard name="Kamal" age={21} homeTown="Negombo"/>
       <StudentCard name="Nimal" age={22} homeTown="Colombo"/>
