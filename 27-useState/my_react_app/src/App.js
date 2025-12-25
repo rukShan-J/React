ca27d3ff1e1e9b04b02393e710b07f9d3fb52eac
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-12-26 00:11:38 +0530

    Add live text preview
    Dynamic text update
    used react

--- 27-useState/my_react_app/src/App.js
+++ 27-useState/my_react_app/src/App.js
@@ -6,12 +6,17 @@ function App() {
 
   const handleChange = (event) =>{
     console.log(event.target.value);
+    const sampleText = document.getElementById('sample-text');
+    sampleText.innerHTML = event.target.value;
   }
 
   return (
     <div>
       <h1>My React App</h1>
       <input type="text" id="normal-text-2" onChange={handleChange}></input>
+      <div>
+        <p id="sample-text"></p>
+      </div>
       <StudentCard name="Amal" age={20} homeTown="Gampaha"/>
       <StudentCard name="Kamal" age={21} homeTown="Negombo"/>
       <StudentCard name="Nimal" age={22} homeTown="Colombo"/>
