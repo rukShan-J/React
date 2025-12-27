8da35fd9c299dada9d4c1c898f104624f96b45ec
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2025-12-27 16:02:50 +0530

    remove direct DOM manipulation and use variable for input value, and update input handling logic

--- 27-useState/my_react_app/src/App.js
+++ 27-useState/my_react_app/src/App.js
@@ -4,10 +4,13 @@ import "./App.css";
 
 function App() {
 
+  let x = 10;
   const handleChange = (event) =>{
     console.log(event.target.value);
-    const sampleText = document.getElementById("sample-text");
-    sampleText.innerHTML = event.target.value;
+    // const sampleText = document.getElementById("sample-text");
+    // sampleText.innerHTML = event.target.value;
+    x = event.target.value;
+    console.log(x);
   }
 
   return (
@@ -15,7 +18,7 @@ function App() {
       <h1>My React App</h1>
       <input type="text" id="normal-text-2" onChange={handleChange}></input>
       <div>
-        <p id="sample-text"></p>
+        <p id="sample-text">{x}</p>
       </div>
       <StudentCard name="Amal" age={20} homeTown="Gampaha"/>
       <StudentCard name="Kamal" age={21} homeTown="Negombo"/>
