043d9c31f2cd7e3618f063438cc289c606bb33d0
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-03 23:41:37 +0530

    use useState to manage input text instead of DOM manipulation
    - Added useState import from React
    - Replaced local variable with state (text)
    - Updated input change handler to update state
    - Rendered input value using state in JSX

--- 27-useState/my_react_app/src/App.js
+++ 27-useState/my_react_app/src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 import StudentCard from "./components/StudentCard.jsx";
 import "./App.css";
 
 function App() {
 
-  let x = 10;
+  const [text, setText] = useState(20);
+  let x = 100;
   const handleChange = (event) =>{
     console.log(event.target.value);
     // const sampleText = document.getElementById("sample-text");
     // sampleText.innerHTML = event.target.value;
     x = event.target.value;
     console.log(x);
+    setText(event.target.value);
   }
 
   return (
@@ -19,6 +21,7 @@ function App() {
       <input type="text" id="normal-text-2" onChange={handleChange}></input>
       <div>
         <p id="sample-text">{x}</p>
+        <p id="sample-text">{text}</p>
       </div>
       <StudentCard name="Amal" age={20} homeTown="Gampaha"/>
       <StudentCard name="Kamal" age={21} homeTown="Negombo"/>
