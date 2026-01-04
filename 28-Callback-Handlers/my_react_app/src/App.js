67740ac8d5b84da346209b2d805efbb1d3b8fa98
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-04 23:20:03 +0530

    copy of project 27... and named as 28...

---
+++ 28-Callback-Handlers/my_react_app/src/App.js
@@ -0,0 +1,33 @@
+import React,{useState} from "react";
+import StudentCard from "./components/StudentCard.jsx";
+import "./App.css";
+
+function App() {
+
+  const [text, setText] = useState(20);
+  let x = 100;
+  const handleChange = (event) =>{
+    console.log(event.target.value);
+    // const sampleText = document.getElementById("sample-text");
+    // sampleText.innerHTML = event.target.value;
+    x = event.target.value;
+    console.log(x);
+    setText(event.target.value);
+  }
+
+  return (
+    <div>
+      <h1>My React App</h1>
+      <input type="text" id="normal-text-2" onChange={handleChange}></input>
+      <div>
+        <p id="sample-text">{x}</p>
+        <p id="sample-text">{text}</p>
+      </div>
+      <StudentCard name="Amal" age={20} homeTown="Gampaha"/>
+      <StudentCard name="Kamal" age={21} homeTown="Negombo"/>
+      <StudentCard name="Nimal" age={22} homeTown="Colombo"/>
+    </div>
+  );
+}
+
+export default App;
