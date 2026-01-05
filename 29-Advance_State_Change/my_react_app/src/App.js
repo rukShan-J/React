4652d86d7b2afd66cc77c1d3b8c046d0c21631d4
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-05 23:01:44 +0530

    copy of project 28... and named as 29...
    removed node_modules folder & babel.js from the copy

---
+++ 29-Advance_State_Change/my_react_app/src/App.js
@@ -0,0 +1,30 @@
+import React,{useState} from "react";
+import StudentCard from "./components/StudentCard.jsx";
+import SearchBar from "./components/searchBar.jsx";
+import "./App.css";
+
+function App() {
+
+  const [text,setText] = useState("10");
+
+  const searchChange = (event) => {
+    console.log("Input changed:", event.target.value);
+    setText(event.target.value);
+  }
+
+  return (
+    <div>
+      <h1>My React App</h1>
+      <SearchBar onSearch={searchChange}/>
+      <div>
+        <p id="sample-text">{text}</p>
+        <p id="sample-text-2"></p>
+      </div>
+      <StudentCard name="Amal" age={20} homeTown="Gampaha"/>
+      <StudentCard name="Kamal" age={21} homeTown="Negombo"/>
+      <StudentCard name="Nimal" age={22} homeTown="Colombo"/>
+    </div>
+  );
+}
+
+export default App;
