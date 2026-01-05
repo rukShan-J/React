32b290bada87b97924635dc14083df4f35358ee2
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-05 22:43:48 +0530

    handle search input in App using useState
    - Added useState hook to manage search text
    - Created searchChange handler to update state
    - Passed searchChange callback to SearchBar via onSearch prop
    - Displayed search text from state in App component

--- 28-Callback-Handlers/my_react_app/src/App.js
+++ 28-Callback-Handlers/my_react_app/src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 import StudentCard from "./components/StudentCard.jsx";
 import SearchBar from "./components/searchBar.jsx";
 import "./App.css";
 
 function App() {
 
+  const [text,setText] = useState("10");
+
   const searchChange = (event) => {
     console.log("Input changed:", event.target.value);
+    setText(event.target.value);
   }
 
   return (
@@ -14,7 +17,7 @@ function App() {
       <h1>My React App</h1>
       <SearchBar onSearch={searchChange}/>
       <div>
-        <p id="sample-text"></p>
+        <p id="sample-text">{text}</p>
         <p id="sample-text-2"></p>
       </div>
       <StudentCard name="Amal" age={20} homeTown="Gampaha"/>
