e57520f6e435b0746135746fbd5d9d3bc56229a0
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-05 20:49:23 +0530

    pass search callback via props and add PropTypes
    - Updated SearchBar to receive onSearch callback as a prop
    - Forwarded input change event to parent handler
    - Removed local console logging from SearchBar
    - Wrapped search input inside a form container
    - Added PropTypes validation for onSearch prop

--- 28-Callback-Handlers/my_react_app/src/components/searchBar.jsx
+++ 28-Callback-Handlers/my_react_app/src/components/searchBar.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const SearchBar = () => {
+const SearchBar = ({onSearch}) => {
 
-    const searchHandler = (event) => {
-        console.log("Searching for:", event.target.value);
-    }
+    const searchHandle = (event) => {
+        // console.log("Searching for:", event.target.value);
+        onSearch(event); // or event.target.value
+    };
     
   return (
-    <div className="form-wrapper">
-      <input type="text" id="search" placeholder="Search for..." onChange={searchHandler}/>
+    <div className="form-container">
+      <form className="form-wrapper">
+        <input 
+          type="text" 
+          id="search" 
+          placeholder="Search for..." 
+          onChange={searchHandle}
+        />
+      </form>
     </div>
   );
+};
+
+SearchBar.propTypes = {
+  onSearch: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
