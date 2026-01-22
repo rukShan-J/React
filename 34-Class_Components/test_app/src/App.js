9f9b7d9e096f25ffcdffa97096e881e016c7f5b3
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-22 22:43:08 +0530

    convert App.js to class component

--- 34-Class_Components/test_app/src/App.js
+++ 34-Class_Components/test_app/src/App.js
@@ -1,2 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+class App extends Component { // This is a class component
+
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit ... <code>src/App.js</code> and save to reload.
+                    </p>
+                </header>
+            </div>
+        );
+    }
+};
+
+export default App;
