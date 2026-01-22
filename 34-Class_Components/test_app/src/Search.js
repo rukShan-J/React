dc67c743661a25cc49e563949b106d46c4a09fe9
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-22 23:28:09 +0530

    convert Search input to controlled component in class component
    - Added local state to Search component to store input text
    - Initialized state with empty string
    - Converted onSearch method to arrow function for proper this binding
    - Updated input onChange handler to update component state
    - Displayed current search text from state

--- 34-Class_Components/test_app/src/Search.js
+++ 34-Class_Components/test_app/src/Search.js
@@ -3,16 +3,21 @@ import { Component } from "react";
 class Search extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            text: ""
+        } 
     }
 
-    onSearch(event) {
+    onSearch = (event) => {
         console.log("Searching for:", event.target.value);
+        this.setState({ text: event.target.value });
     }
 
     render() {
         return (
             <div>
                 <input type="text" placeholder="Search..." onChange={this.onSearch} />
+                <p>{this.state.text}</p>
             </div>
         );
     }
