eb75386e4c86611e7c3d2d96912783cbf4151fd8
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-23 23:03:03 +0530

    convert Student component from functional to class component
    - Replaced functional Student component with class-based version
    - Imported and extended React Component
    - Added constructor with super(props)
    - Accessed props using this.props in render method
    - Destructured name, age, and homeTown from props
    - Kept existing UI output unchanged
    - Commented out old functional implementation for reference

--- 35-Convert_Functionl_to_Class/my_react_app/src/components/Student.jsx
+++ 35-Convert_Functionl_to_Class/my_react_app/src/components/Student.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React,{Component} from "react";
 import PropTypes from "prop-types";
 
-const Student = ({name,age,homeTown}) => {
-    return(
-        <div>
-            <h3>{name}</h3>
-            <h3>{age}</h3>
-            <h3>{homeTown}</h3>
-        </div>
-    )
-};
+class Student extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const{name,age,homeTown} = this.props;
+        return(
+            <div>
+                <h3>{name}</h3>
+                <h3>{age}</h3>
+                <h3>{homeTown}</h3>
+            </div>
+        );
+    }
+}
+
+// const Student = ({name,age,homeTown}) => {
+//     return(
+//         <div>
+//             <h3>{name}</h3>
+//             <h3>{age}</h3>
+//             <h3>{homeTown}</h3>
+//         </div>
+//     )
+// };
 
 Student.propTypes = {
     name: PropTypes.string,
