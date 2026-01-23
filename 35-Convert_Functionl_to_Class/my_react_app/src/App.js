b42007d49f03827d9ce8450c2816ba1e12f123e2
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-23 22:29:51 +0530

    convert App from functional to class component
    - Replaced useState hook with class-based state
    - Converted App to extend React Component
    - Initialized state using constructor
    - Moved searchChange logic to class method
    - Updated SearchBar usage to use class handler and state
    - Implemented name-based student filtering using class state
    - Rendered filtered StudentCard list from class component
    - Kept old functional component code commented for reference

--- 35-Convert_Functionl_to_Class/my_react_app/src/App.js
+++ 35-Convert_Functionl_to_Class/my_react_app/src/App.js
@@ -1,61 +1,117 @@
-import React,{useState} from "react";
+import React,{Component} from "react";
 import StudentCard from "./components/StudentCard.jsx";
 import SearchBar from "./components/searchBar.jsx";
 import "./App.css";
 
-class
-
-function App() {
-
-  const [text,setText] = useState("k");
-
-  const studentList = [
-    {name:"Dasun", age:17, town:"Gampaha"},
-    {name:"Kasun", age:18, town:"Negombo"},
-    {name:"Pasan", age:19, town:"Colombo"}
-  ]
-
-  // Filtered students whose age is greater than input value
-  // const searchedStudents = studentList.filter((student) => (student.age > text));
-
-  // Filtered students by name
-  const searchedStudents = studentList.filter((student) => 
-    student.name.toLowerCase().includes(text.toLowerCase())
-  );
-
-  const searchChange = (event) => {
-    console.log("Input changed:", event.target.value);
-    setText(event.target.value);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text:""
+    };
   }
 
-  return (
-    <div>
-      <h1>My React App</h1>
-      <SearchBar onSearch={searchChange} searchText={text}/>
-      <div>
-        <p id="sample-text">{text}</p>
-        <p id="sample-text-2"></p>
-      </div>
+  searchChange = (event) => {
+  console.log("Input changed:", event.target.value);
+  this.setState({text:event.target.value});
+  }
 
-      {
-        searchedStudents.map((student,index)=>{
-          return(
-            <
-              StudentCard
-              key={index}
-              name={student.name}
-              age={student.age}
-              homeTown={student.town}
-            />
-          )
-        })
-      }
+  render() {
+    const {text} = this.state;
+
+    const studentList = [
+      {name:"Dasun", age:17, town:"Gampaha"},
+      {name:"Kasun", age:18, town:"Negombo"},
+      {name:"Pasan", age:19, town:"Colombo"}
+    ];
+
+    // Filtered students by name
+    const searchedStudents = studentList.filter((student) => 
+      student.name.toLowerCase().includes(this.state.text.toLowerCase())
+    );
+
+    return (
+      <div>
+        <h1>My React App</h1>
+        <SearchBar onSearch={this.searchChange} searchText={this.text}/>
+        <div>
+          <p id="sample-text">{text}</p>
+          <p id="sample-text-2"></p>
+        </div>
+
+        {
+          searchedStudents.map((student,index)=>{
+            return(
+              <StudentCard
+                key={index}
+                name={student.name}
+                age={student.age}
+                homeTown={student.town}
+              />
+            )
+          })
+        }
       
       {/* <StudentCard name="Amal" age={20} homeTown="Gampaha"/>
       <StudentCard name="Kamal" age={21} homeTown="Negombo"/>
       <StudentCard name="Nimal" age={22} homeTown="Colombo"/> */}
     </div>
-  );
+    )
+    
+  }
 }
+
+// function App() {
+
+//   const [text,setText] = useState("k");
+
+//   const studentList = [
+//     {name:"Dasun", age:17, town:"Gampaha"},
+//     {name:"Kasun", age:18, town:"Negombo"},
+//     {name:"Pasan", age:19, town:"Colombo"}
+//   ];
+
+//   // Filtered students whose age is greater than input value
+//   // const searchedStudents = studentList.filter((student) => (student.age > text));
+
+//   // Filtered students by name
+//   const searchedStudents = studentList.filter((student) => 
+//     student.name.toLowerCase().includes(text.toLowerCase())
+//   );
+
+//   const searchChange = (event) => {
+//     console.log("Input changed:", event.target.value);
+//     setText(event.target.value);
+//   }
+
+//   return (
+//     <div>
+//       <h1>My React App</h1>
+//       <SearchBar onSearch={searchChange} searchText={text}/>
+//       <div>
+//         <p id="sample-text">{text}</p>
+//         <p id="sample-text-2"></p>
+//       </div>
+
+//       {
+//         searchedStudents.map((student,index)=>{
+//           return(
+//             <
+//               StudentCard
+//               key={index}
+//               name={student.name}
+//               age={student.age}
+//               homeTown={student.town}
+//             />
+//           )
+//         })
+//       }
+      
+//       {/* <StudentCard name="Amal" age={20} homeTown="Gampaha"/>
+//       <StudentCard name="Kamal" age={21} homeTown="Negombo"/>
+//       <StudentCard name="Nimal" age={22} homeTown="Colombo"/> */}
+//     </div>
+//   );
+// }
 
 export default App;
