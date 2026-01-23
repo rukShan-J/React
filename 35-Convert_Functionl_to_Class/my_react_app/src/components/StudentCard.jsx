1c058b31a69a694e223af9286a7d4ab8299ce879
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-23 23:09:13 +0530

    convert StudentCard from functional to class component
    - Replaced functional StudentCard with class-based component
    - Imported and extended React Component
    - Added constructor with super(props)
    - Accessed props using this.props in render method
    - Destructured name, age, and homeTown from props
    - Kept existing card layout and Student component usage unchanged
    - Commented out old functional implementation for reference

--- 35-Convert_Functionl_to_Class/my_react_app/src/components/StudentCard.jsx
+++ 35-Convert_Functionl_to_Class/my_react_app/src/components/StudentCard.jsx
@@ -1,18 +1,37 @@
 import Student from "./Student.jsx";
-import React from "react";
+import React,{Component} from "react";
 import PropTypes from "prop-types";
 
-const StudentCard = ({name,age,homeTown}) => {
-    return(
-        <div className="st-card">
-            <img 
-            src="https://i.pinimg.com/736x/9e/43/2d/9e432dd715a57ffa3f7759a4e569655a.jpg" 
-            alt={`Student Image_${name}`} 
-            className="student-image" />
-            <Student name={name} age={age} homeTown={homeTown}/>
-        </div>
-    );
-};
+class StudentCard extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const{name,age,homeTown} = this.props;
+        return(
+            <div className="st-card">
+                <img 
+                src="https://i.pinimg.com/736x/9e/43/2d/9e432dd715a57ffa3f7759a4e569655a.jpg" 
+                alt={`Student Image_${name}`} 
+                className="student-image" />
+                <Student name={name} age={age} homeTown={homeTown}/>
+            </div>
+        );
+    }
+}
+
+// const StudentCard = ({name,age,homeTown}) => {
+//     return(
+//         <div className="st-card">
+//             <img 
+//             src="https://i.pinimg.com/736x/9e/43/2d/9e432dd715a57ffa3f7759a4e569655a.jpg" 
+//             alt={`Student Image_${name}`} 
+//             className="student-image" />
+//             <Student name={name} age={age} homeTown={homeTown}/>
+//         </div>
+//     );
+// };
 
 StudentCard.propTypes = {
     name: PropTypes.string,
