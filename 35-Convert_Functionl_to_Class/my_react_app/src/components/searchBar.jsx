9d7dcd6e12d597001067b178ee41c993c8240a68
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-23 22:57:52 +0530

    convert SearchBar from functional to class component
    - Replaced useState hook with class-based state
    - Converted App to extend React Component
    - Initialized state using constructor
    - Moved searchChange logic to class method
    - Updated SearchBar usage to use class handler and state
    - Implemented name-based student filtering using class state
    - Rendered filtered StudentCard list from class component
    - Kept old functional component code commented for reference

--- 35-Convert_Functionl_to_Class/my_react_app/src/components/searchBar.jsx
+++ 35-Convert_Functionl_to_Class/my_react_app/src/components/searchBar.jsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React,{Component} from "react";
 import PropTypes from "prop-types";
 
-const SearchBar = ({onSearch, searchText}) => {
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-    const searchHandle = (event) => {
+    searchHandle = (event) => {
         // console.log("Searching for:", event.target.value);
-        onSearch(event); // or event.target.value
+        this.props.onSearch(event); // or event.target.value
     };
+
+    render() {
+      return (
+        <div className="form-container">
+          <form className="form-wrapper">
+            <input 
+              type="text" 
+              id="search" 
+              value={this.props.searchText}
+              placeholder="Search for..." 
+              onChange={this.searchHandle}
+            />
+          </form>
+        </div>
+      );
+    }
+}
+
+// const SearchBar = ({onSearch, searchText}) => {
+
+  //   const searchHandle = (event) => {
+  //       // console.log("Searching for:", event.target.value);
+  //       onSearch(event); // or event.target.value
+  //   };
     
-  return (
-    <div className="form-container">
-      <form className="form-wrapper">
-        <input 
-          type="text" 
-          id="search" 
-          value={searchText}
-          placeholder="Search for..." 
-          onChange={searchHandle}
-        />
-      </form>
-    </div>
-  );
-};
+  // return (
+  //   <div className="form-container">
+  //     <form className="form-wrapper">
+  //       <input 
+  //         type="text" 
+  //         id="search" 
+  //         value={searchText}
+  //         placeholder="Search for..." 
+  //         onChange={searchHandle}
+  //       />
+  //     </form>
+  //   </div>
+  // );
+// };
 
 SearchBar.propTypes = {
   onSearch: PropTypes.func.isRequired,
