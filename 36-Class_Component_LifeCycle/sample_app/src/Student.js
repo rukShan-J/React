f5028cc147ad61e9d676b047b293ac345bc3c78b
Pubudu Rukshan Jayananda <jayananda.apr@gmail.com>
2026-01-25 18:30:49 +0530

    add getDerivedStateFromProps lifecycle to Student component
    - Added constructor to initialize local state
    - Initialized year value in component state
    - Implemented static getDerivedStateFromProps lifecycle method
    - Derived year value based on current age prop
    - Prevented unnecessary state updates by returning null when unchanged
    - Displayed derived state value in the UI
    - Added console logs to observe lifecycle behavior

--- 36-Class_Component_LifeCycle/sample_app/src/Student.js
+++ 36-Class_Component_LifeCycle/sample_app/src/Student.js
@@ -2,8 +2,21 @@ import { Component } from 'react';
 
 class Student extends Component {
   
+    //constructor(props) function - used to initialize the state and bind methods
     constructor(props) {
         super(props);
+        this.state = {year: 0};
+    }
+
+    //static getDerivedStateFromProps() - invoked right before calling the render method, both on the initial mount and on subsequent updates
+    static getDerivedStateFromProps(nextProps, prevState) {
+        const newYear = new Date().getFullYear() - nextProps.age; //calculate the year of birth
+        console.log("getDerivedStateFromProps - ", newYear);
+        if(newYear === prevState.year){
+            return null; //no change to state
+        }else{
+            return ({year: newYear}); //update state
+        }
     }
 
     render() {
@@ -14,6 +27,7 @@ class Student extends Component {
             <div>
                 <h2>Name - {name}</h2>{/* displaying props data */}
                 <h2>Age - {age}</h2>
+                <h2>Year - {this.state.year}</h2>{/* displaying state data */}
             </div>
         );
     }
